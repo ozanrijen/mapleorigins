b20d0c5c5c5108704e3acc9756018d58b10b1419
Yeag <jmardell263@gmail.com>
2019-09-15 20:14:36 +0100

    Remove the forced java version
    The server will now use the systems default java version to run the server (Should be Java 10, if you dont have Java 10 redownload now)

--- launch.bat
+++ launch.bat
@@ -1,6 +1,5 @@
 @echo off
 @title MapleOrigins
-set PATH=C:\Program Files\Java\jdk1.8.0_211\bin;%PATH%
-set CLASSPATH=.;dist\*
+set CLASSPATH=.;dist\*;cores\*
 java -Xmx2048m -Dwzpath=wz\ net.server.Server
 pause
