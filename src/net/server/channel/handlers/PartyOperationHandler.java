8137d577f5e5896e527de213490566046f43654a
Yeag <jmardell263@gmail.com>
2019-09-16 14:23:48 +0100

    Fixed party invites for lower levels

--- src/net/server/channel/handlers/PartyOperationHandler.java
+++ src/net/server/channel/handlers/PartyOperationHandler.java
@@ -77,11 +77,7 @@ public final class PartyOperationHandler extends AbstractMaplePacketHandler {
                 String name = slea.readMapleAsciiString();
                 MapleCharacter invited = world.getPlayerStorage().getCharacterByName(name);
                 if (invited != null) {
-                    if(invited.getLevel() < 10 && (!ServerConstants.USE_PARTY_FOR_STARTERS || player.getLevel() >= 10)) { //min requirement is level 10
-                        c.announce(MaplePacketCreator.serverNotice(5, "The player you have invited does not meet the requirements."));
-                        return;
-                    }
-                    if(ServerConstants.USE_PARTY_FOR_STARTERS && invited.getLevel() >= 10 && player.getLevel() < 10) {    //trying to invite high level
+                    if (!ServerConstants.USE_PARTY_FOR_STARTERS && invited.getLevel() < 10) { //min requirement is level 10
                         c.announce(MaplePacketCreator.serverNotice(5, "The player you have invited does not meet the requirements."));
                         return;
                     }
